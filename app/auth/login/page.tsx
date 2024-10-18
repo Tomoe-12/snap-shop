c6df07cf34b2caadb4026267d5042477bfddef6f
Tomoe-12 <li8993han@gmail.com>
2024-10-18 10:11:02 +0630

    login form ui

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
 import AuthForm from "@/components/auth/auth-form";
 import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/type/login-schema";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 const Login = () => {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
+    console.log("values : ", values);
+  };
+
   return (
     <div>
       <AuthForm
@@ -10,7 +39,42 @@ const Login = () => {
         footerHerf="/auth/register"
         showProvider
       >
-        <h2>Login Form</h2>
+        <Form {...form}>
+          <div className="space-y-3">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="snapLikeADoo@gmail.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="******" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button variant={"link"} size={'sm'} className="pl-0 mb-1" >
+              <Link href={"/auth/reset"}>Forget Password</Link>
+            </Button>
+          </div>
+
+          <Button className="w-full mb-4">Login</Button>
+        </Form>
       </AuthForm>
     </div>
   );
