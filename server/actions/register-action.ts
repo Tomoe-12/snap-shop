bc13400f2faa1a26490fc0b7cf23937df3bc8e97
Tomoe-12 <li8993han@gmail.com>
2024-10-21 05:25:40 +0630

    generating  expires token

--- server/actions/register-action.ts
+++ server/actions/register-action.ts
@@ -6,27 +6,41 @@ import bcrypt from "bcrypt";
 import { db } from "..";
 import { users } from "../schema";
 import { eq } from "drizzle-orm";
+import { generateEmailVerificationToken } from "./token";
 
 export const register = actionClient
   .schema(registerSchema)
   .action(async ({ parsedInput: { name, email, password } }) => {
     const hashpassword = await bcrypt.hash(password, 10);
-    console.log(hashpassword);
-    
+
     // check user exists or not
     const existingUser = await db.query.users.findFirst({
       where: eq(users.email, email),
     });
 
     if (existingUser) {
-      if (!existingUser.emailVerified)
-        // send verification email
-      return {success : "Email verification sent "}
+      if (!existingUser.emailVerified) {
+        const verificationToken = await generateEmailVerificationToken(email);
+
+        return { success: "Email verification resent." };
+      }
+
+      // send verification email
+      return { error: "Email is already exists " };
     }
 
+    // record user
+    await db.insert(users).values({
+      name,
+      email,
+      password: hashpassword,
+    });
+    // generate verification token for email expires in 30 mins
+    const verificationToken = await generateEmailVerificationToken(email);
+    // send verification token
+    console.log("verficaiont email", verificationToken);
 
-    // create user
     return {
-      success: "Email verification sent to your email.",
+      success: "Email verification sent.",
     };
   });
