67ff25c4f4779694a5c89d1f08d505f7235f6108
Tomoe-12 <li8993han@gmail.com>
2024-10-27 20:30:31 +0630

    login

--- server/actions/login-action.ts
+++ server/actions/login-action.ts
@@ -2,13 +2,56 @@
 
 import { loginSchema } from "@/types/login-schema";
 import { actionClient } from "./safe-action";
+import { db } from "..";
+import { users } from "../schema";
+import { eq } from "drizzle-orm";
+import { generateEmailVerificationToken } from "./token";
+import { sendEmail } from "./email";
+import { signIn } from "../auth";
+import { AuthError } from "next-auth";
 
 export const login = actionClient
   .schema(loginSchema)
   .action(async ({ parsedInput: { email, password } }) => {
-    console.log(email, password);
+    try {
+      // check email
+      const existingUser = await db.query.users.findFirst({
+        where: eq(users.email, email),
+      });
 
-    return {
-      success: { email, password },
-    };
+      console.log(existingUser);
+
+      if (existingUser?.email !== email) {
+        return { error: "Please provide valid credientials" };
+      }
+
+      if (!existingUser.emailVerified) {
+        const verificationToken = await generateEmailVerificationToken(
+          existingUser.email
+        );
+        await sendEmail(
+          verificationToken[0].email,
+          verificationToken[0].token,
+          existingUser?.name?.split(" ")[0]! // return firstname
+        );
+        return { success: "Email verification resent." };
+      }
+
+       await signIn("credentials", {
+        email,
+        password,
+        redirectTo: "/",
+      });
+
+      return { success: "Login Successfully " };
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case "CredentialsSignin":
+            return { error: "Please provide valid credientials" };
+          case "OAuthSignInError":
+            return { error: error.message };
+        }
+      }
+    }
   });
--- server/auth.ts
+++ server/auth.ts
@@ -1,21 +1,53 @@
-import NextAuth from "next-auth"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "@/server"
-import Github from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
- 
+import NextAuth, { CredentialsSignin } from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import bcrypt from 'bcrypt'
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { loginSchema } from "@/types/login-schema";
+import { db } from ".";
+import { users } from "./schema";
+import { eq } from "drizzle-orm";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db) as any,
-  secret : process.env.AUTH_SECRET!,
-  session : {strategy  : "jwt"},
+  secret: process.env.AUTH_SECRET!,
+  session: { strategy: "jwt" },
   providers: [
-    Google ( {
-        clientId : process.env.AUTH_GOOGLE_ID!,
-        clientSecret : process.env.AUTH_GOOGLE_SECRET!,
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     }),
     Github({
-        clientId : process.env.AUTH_GITHUB_ID!,
-        clientSecret : process.env.AUTH_GITHUB_SECRET!,
-    })
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Credentials({
+      authorize: async (credentials) => {
+        try {
+          const validatedData = loginSchema.safeParse(credentials);
+          
+          if (validatedData.error) {
+            return null;
+          }
+          const { email, password } = validatedData.data;
+          
+          const user = await db.query.users.findFirst({
+            where: eq(users.email, email),
+          });
+
+          if (!user || !password) {
+            return null;
+          }
+          
+          const isMatch = await bcrypt.compare(password, user?.password!);
+          
+          if (!isMatch) return null;
+
+          return user;
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
   ],
-})
+});
