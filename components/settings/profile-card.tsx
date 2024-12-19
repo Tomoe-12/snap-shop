6c80f4eddf0ebca8562cd4eeb24d7a58e914ac18
Tomoe-12 <li8993han@gmail.com>
2024-12-19 14:04:03 +0630

    dashboard nav

--- components/settings/profile-card.tsx
+++ components/settings/profile-card.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import { Session } from "next-auth";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SettingsCard from "./settings-card";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 import { UserRoundPen } from "lucide-react";
+
 import {
   Dialog,
   DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "../ui/dialog";
-import { Button } from "../ui/button";
-import useMediaQuery from "@/hooks/useMediaQuery";
+} from "@/components/ui/dialog";
+
 import {
   Drawer,
   DrawerClose,
@@ -27,91 +27,92 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+
+import { Button } from "../ui/button";
+
+import useMediaQuery from "@/hooks/useMediaQuery";
 import ProfileForm from "./profile-form";
 import AvatarUploadForm from "./avatar-upload-form";
+
 type ProfileCardProps = {
   session: Session;
 };
-
 const ProfileCard = ({ session }: ProfileCardProps) => {
   const isDesktop = useMediaQuery("(min-width: 768px)");
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleSetOpen = () => {
-    setOpen(false);
+  const handleisOpen = () => {
+    setIsOpen(false);
   };
 
   return (
     <SettingsCard>
-      <div className="flex items-start justify-between">
-        <div className="flex items-start gap-3">
+      <div className="flex items-start gap-2 justify-between">
+        <div className="flex items-start gap-2 flex-col lg:flex-row">
           <AvatarUploadForm
-            name={session?.user?.name!}
-            image={session?.user?.image}
-            email={session?.user?.email!}
+            name={session.user?.name}
+            image={session.user?.image}
+            email={session.user.email}
           />
           <div>
-            <p className="text-sm font-medium text-muted-foreground ">
+            <p className="text-sm font-medium text-muted-foreground">
               Display Name
             </p>
-            <h2 className="font-medium text-lg">{session?.user?.name}</h2>
-            <p className="text-sm font-medium text-muted-foreground ">Email</p>
-            <p className="text-base font-medium">
-              {session?.user?.email}
+            <h2 className=" font-medium text-sm lg:text-base">
+              @{session?.user?.name}
+            </h2>
+            <p className="text-sm font-medium text-muted-foreground mt-2">
+              Email:
+            </p>
+            <p className="text-sm lg:text-base font-medium">
+              {session.user?.email}
             </p>
           </div>
         </div>
-
         {isDesktop ? (
-          <Dialog open={open} onOpenChange={setOpen}>
+          <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogTrigger asChild>
-              <UserRoundPen
-                size={24}
-                className="text-muted-foreground hover:text-black cursor-pointer"
-              />
+              <UserRoundPen className="w-5 h-5 text-muted-foreground hover:text-black cursor-pointer" />
             </DialogTrigger>
             <DialogContent className="mx-4 lg:mx-0">
               <DialogHeader>
-                <DialogTitle>Edit Profile </DialogTitle>
+                <DialogTitle>Edit Profile</DialogTitle>
                 <DialogDescription>
                   This will be your public display name.
                 </DialogDescription>
               </DialogHeader>
               <ProfileForm
-                handleSetOpen={handleSetOpen}
-                name={session?.user?.name!}
-                email={session?.user?.email!}
+                name={session.user?.name!}
+                email={session.user?.email!}
+                setIsOpen={handleisOpen}
               />
               <DialogClose asChild>
-                <Button variant={"outline"} className="w-full">
+                <Button variant="outline" className="w-full">
                   Cancel
                 </Button>
               </DialogClose>
             </DialogContent>
           </Dialog>
         ) : (
-          <Drawer open={open} onOpenChange={setOpen}>
+          <Drawer open={isOpen} onOpenChange={setIsOpen}>
             <DrawerTrigger asChild>
-              <UserRoundPen
-                size={24}
-                className="text-muted-foreground hover:text-black cursor-pointer"
-              />
+              <UserRoundPen className="w-5 h-5 text-muted-foreground hover:text-black cursor-pointer" />
             </DrawerTrigger>
             <DrawerContent>
-              <DrawerHeader className="text-left">
+              <DrawerHeader>
                 <DrawerTitle>Edit Profile</DrawerTitle>
                 <DrawerDescription>
                   This will be your public display name.
                 </DrawerDescription>
               </DrawerHeader>
               <ProfileForm
-                handleSetOpen={handleSetOpen}
-                name={session?.user?.name!}
-                email={session?.user?.email!}
+                name={session.user?.name!}
+                email={session.user?.email!}
+                setIsOpen={handleisOpen}
               />
               <DrawerFooter>
-                <DrawerClose asChild>
-                  <Button variant={"outline"} className="w-full">
+                <DrawerClose>
+                  <Button variant="outline" className="w-full">
                     Cancel
                   </Button>
                 </DrawerClose>
