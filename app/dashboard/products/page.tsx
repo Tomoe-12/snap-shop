ec4a647bb395acfc42f3c0f27a8179ff1fa240e1
Tomoe-12 <li8993han@gmail.com>
2024-12-25 20:11:46 +0630

    tanstack datatable configuration

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,7 +1,26 @@
+import { db } from "@/server";
 import React from "react";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+import myMan from '../../../public/91041aca4100e4743ba56f6f70497884.jpg'
+const Products = async () => {
+  const products = await db.query.products.findMany({
+    orderBy: (products, { desc }) => [desc(products.id)],
+  });
 
-const Products = () => {
-  return <div>Products</div>;
+  const productData = products.map((product)=>{
+    return {
+      id : product.id ,
+      price : product.price,
+      title : product.title ,
+      description : product.description ,
+      variants : [],
+    image : myMan.src
+    }
+  })
+  return <main>
+    <DataTable columns={columns} data={productData} />
+  </main>
 };
 
 export default Products;
