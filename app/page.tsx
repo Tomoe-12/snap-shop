a5abff92320506f7beec12ece3aaf8e334624fce
Tomoe-12 <li8993han@gmail.com>
2025-03-30 21:16:42 +0630

    built home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+import Products from "@/components/products";
 import { Button } from "@/components/ui/button";
+import { db } from "@/server";
+import { productVariants } from "@/server/schema";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const productsWithVariants = await db.query.productVariants.findMany({
+    with: {
+      variantImages: true,
+      variantTags: true,
+      product: true,
+    },
+    orderBy: (productVariants, { desc }) => [desc(productVariants.id)],
+  });
+
+  console.log("productsWithVariants", productsWithVariants);
+  
   return (
    <main>
-    <Button>Hello world</Button>
+    <h2>Nav</h2>
+    <Products productsWithVariants={productsWithVariants} />
    </main>
   );
 }
---
+++ components/products/index.tsx
@@ -0,0 +1,41 @@
+import formatCurrency from "@/lib/formatCurrency";
+import { VariantsWithProduct } from "@/lib/infer-type";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type ProductsProps = {
+  productsWithVariants: VariantsWithProduct[];
+};
+const Products = ({ productsWithVariants }: ProductsProps) => {
+  console.log("productsWithVariants", productsWithVariants);
+
+  return (
+    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {productsWithVariants.map((product) => (
+        <Link
+          href={`/products/${product.id}?productId=${product.product.id}&type=${product.productType}&image=${product.variantImages[0].image_url}&title=${product.product.title}&price=${product.product.price} `}
+          key={product.id}
+          className="bg-white p-4 rounded-md shadow-md"
+        >
+          <Image
+            src={product.variantImages[0].image_url}
+            alt={product.product.title}
+            width={400}
+            height={300}
+          />
+          <hr className="my-2" />
+          <h3 className="font-semibold  ">
+            {product.product.title.substring(0, 25)}
+          </h3>
+          <p>{product.product.description}</p>
+          <p className="font-medium text-sm mt-1">
+            {formatCurrency(product.product.price)} MMK
+          </p>
+        </Link>
+      ))}
+    </main>
+  );
+};
+
+export default Products;
